aef06f760a88f6e2135a2196509c4dd2e4c99145
bmgwaro <brian.gwaro@student.moringaschool.com>
2024-06-25 22:25:42 +0300

    functions js

--- index.js
+++ index.js
@@ -1,1 +1,33 @@
 // Code your solution in this file!
+const hq = 42;
+const block=264;
+
+function distanceFromHqInBlocks(location) {
+  return Math.abs(location - hq);
+}
+
+function distanceFromHqinFeet(location) {
+  const noOfBlocks=distanceFromHqInBlocks(location);
+  return noOfBlocks*block;
+}
+
+function distanceTravelledInFeet(start, end) {
+    const noOfBlocks=Math.abs(end-start);
+    return noOfBlocks*block;
+}
+
+function calculatesFarePrice(start, end) {
+    const feetDistance=distanceTravelledInFeet(start, end) 
+    if(feetDistance <= 400){
+        return 0;
+    }
+    else if(feetDistance > 400 && feetDistance <= 2000) {
+        return (feetDistance - 400) * 0.02;
+    }
+    else if(feetDistance > 2000 && feetDistance <= 2500) {
+        return 25;
+    }
+    else if(feetDistance > 2500) {
+        return "cannot travel that far"
+    }
+}
